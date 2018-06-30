3bc9e5e1a06fe9a8c47397cc75f99924237f60c2
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-06-30 16:49:05 -0400

    Clearer documentation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+################################################################################
+# This is the simplest code that will allow you to fetch from a repository that
+# requires username-password authentication.
+################################################################################
 import pygit2
-from pygit2 import Repository, GitError, RemoteCallbacks, UserPass, Username, GIT_CREDTYPE_USERNAME
 import os
 import getpass
 
@@ -7,8 +10,12 @@ import getpass
 # The repo is https://github.com/EscherCone which is a repo that is on my
 # computer but is private on github.
 ################################################################################
+# it is at this path
 repo_path = os.path.join(os.path.expanduser('~'), 'Documents', 'GitHub', 'EscherCone')
-repo_object = Repository(os.path.join(repo_path, '.git'))
+# the Repository contructor wants the .git directory
+repo_object = pygit2.Repository(os.path.join(repo_path, '.git'))
+# Remotes are the objects that allow us to do fetch and push for the remote that
+# it is.
 origin_object = repo_object.remotes['origin']
 
 ################################################################################
@@ -38,27 +45,28 @@ origin_object = repo_object.remotes['origin']
 # password.  THere are two others fo doing different types of authentication
 ################################################################################
 
-# Our object that groups some callbacks that will get called
+# Our object that groups some callbacks that will get called.  An instance of
+# this will be passed as the 'callbacks' parameter of the fetch method.
 class MyCreds(pygit2.RemoteCallbacks):
-    @staticmethod
-    def credentials(url, username_from_url, allowed_types):
+    # It has to implement some methods, but we're only going to use it in a way
+    # where this method will be called.
+    def credentials(self, url, username_from_url, allowed_types):
+        print(self, url, username_from_url, allowed_types)
         # allowed_types tells us what type of Credential object the server wants
         if allowed_types == pygit2.GIT_CREDTYPE_USERPASS_PLAINTEXT:
             # Our problem: do what you need to do in order to find out what to
             # put into the UserPass object.
             password = getpass.getpass('Enter password for url={}'.format(url))
-            return UserPass('philippecarphin', password)
-        # Deal with other types of credential requirements
-        # elif allowed_types == pygit2.GITCREDTYPE_USERNAME:
-        #    raise NotImplemented
-
-
-# So here's an instance of this thing we just made
-my_remote_callback = RemoteCallbacks(credentials=MyCreds.credentials)
+            return pygit2.UserPass('philippecarphin', password)
+        elif allowed_types == pygit2.GITCREDTYPE_USERNAME:
+            raise NotImplemented
 
 ################################################################################
 # And that's it!
 # Doing the fetch and giving it the callback.  It returns a
 # pygit2.TransferProgress.  Anyway the point is calling fetch with the callback.
 ################################################################################
-transfer_process_thingy= origin_object.fetch(callbacks=my_remote_callback)
+transfer_process_thingy = origin_object.fetch(callbacks=MyCreds())
+                                            #           ^^^^^^^^^
+                                            # pass an instance of MyCreds as
+                                            # the paramater callbacks.
